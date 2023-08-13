f67a9c8d59e8f310932c34bd5e16336c68828dfb
Toshiyuki Imaizumi <evening_glow@khf.biglobe.ne.jp>
2023-08-13 20:27:17 +0900

    3層のニューラルネットワークのサンプル追加

--- deep_learning_example/deep_learning_exmaple/__main__.py
+++ deep_learning_example/deep_learning_exmaple/__main__.py
@@ -5,20 +5,21 @@ import perceptron as per
 import neural_network as nn
 
 def main():
-    print( per.AND(1, 1))
-    print( per.AND(0, 1))
+    print('AND(1,1):' + str(per.AND(1, 1)))
+    print('AND(0,1):' + str(per.AND(0, 1)))
     
-    print( per.AND_simple(1, 1))
-    print( per.AND_simple(0, 1))
+    print('AND(1,1):' + str( per.AND_simple(1, 1)))
+    print('AND(0,1):' + str( per.AND_simple(0, 1)))
     
     x = np.array([0,1])
     w = np.array([0.5,0.5])
     b = -0.7
-    print( np.sum(w*x) + b)
+    print('perceptron sample:' +str(np.sum(w*x) + b))
     
     nn.show_step_function_sample()
     nn.show_sigmoid_function_sample()
     nn.show_relu_function_sample()
+    nn.show_sample_network()
 
 
 if __name__ == "__main__":
--- deep_learning_example/deep_learning_exmaple/neural_network.py
+++ deep_learning_example/deep_learning_exmaple/neural_network.py
@@ -19,6 +19,9 @@ def sigmoid(x):
 def relu(x):
     return np.maximum(0,x)
 
+def identity_function(x):
+    return x
+
 def show_step_function_sample():
     x = np.arange(-5.0,5.0,0.1)
     y = step_function(x)
@@ -39,3 +42,43 @@ def show_relu_function_sample():
     plt.plot(x, y)
     plt.ylim(-0.1, 6.1) # y軸の範囲
     plt.show()
+    
+#入力層2、第1層3、第2層2、出力層2のニューラルネットワーク
+def init_network():
+    network = {}
+    network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+    network['b1'] = np.array([0.1, 0.2, 0.3])
+    network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+    network['b2'] = np.array([0.1, 0.2])
+    network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
+    network['b3'] = np.array([0.1, 0.2])
+    
+    return network
+
+def forward(network, x):
+    W1,W2,W3 = network['W1'], network['W2'], network['W3']
+    b1,b2,b3 = network['b1'], network['b2'], network['b3']
+    
+    #print('x =' + str(x))
+    #print('W1 =' + str(W1))
+    #print(x.shape)
+    #print(W1.shape)
+    
+    a1 = np.dot(x, W1) + b1
+    print('a1:' + str(a1)) #1層の入力
+    z1 = sigmoid(a1)
+    print('z1:' + str(z1)) #1層の出力結果
+    a2 = np.dot(z1, W2) + b2
+    print('a2:' + str(a2)) #1層の入力
+    z2 = sigmoid(a2)
+    print('z2:' + str(z2)) #2層の出力結果
+    a3 = np.dot(z2, W3) + b3
+    y = identity_function(a3)
+    
+    return y
+
+def show_sample_network():
+    network = init_network()
+    x = np.array([1.0, 0.5])
+    y = forward(network, x)
+    print('y:'+str(y))
