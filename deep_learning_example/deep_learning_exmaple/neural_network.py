da3d2abb335b1d205937f70e31c81bdf6425dc51
Toshiyuki Imaizumi <evening_glow@khf.biglobe.ne.jp>
2024-04-28 19:17:03 +0900

    softmax関数の不備修正

--- deep_learning_example/deep_learning_exmaple/neural_network.py
+++ deep_learning_example/deep_learning_exmaple/neural_network.py
@@ -32,12 +32,16 @@ def relu(x):
 def identity_function(x):
     return x
 
-def softmax(a):
+def simple_softmax(a):
     c = np.max(a)
     exp_a = np.exp(a - c) #オーバーフロー対策で入力値の最大値を引いておく
     sum_exp_a = np.sum(exp_a)
     y = exp_a / sum_exp_a
     return y
+
+def softmax(x):
+    x = x - np.max(x, axis=-1, keepdims=True)   # オーバーフロー対策
+    return np.exp(x) / np.sum(np.exp(x), axis=-1, keepdims=True)
     
 # 2乗和誤差。値が小さいほど正解に近い
 # y=ニューラルネットワークの出力、t=教師データの想定
